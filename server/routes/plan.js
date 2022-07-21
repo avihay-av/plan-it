2335f86ab9b37c1c8ca400100d1b8b64fc32fccc
Orihassid <Orihassid123@gmail.com>
2022-07-21 12:37:25 +0300

    feat: get call to test server

--- server/routes/plan.js
+++ server/routes/plan.js
@@ -1,5 +1,15 @@
 const express  = require("express");
-
+const {
+  
+  getAllItems,
+ 
+} = require( "../controllers/planController.js")
 const planRouter = express.Router();
+
+planRouter.get("/", (req,res)=>{
+    res.status(200).json("test");
+    
+});
+
 
 module.exports =  planRouter;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
-const express =  require('express');
-const  cors = require('cors');
-const  bodyParser = require( 'body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const planRouter = require("./routes/plan.js");
 
-const  port = 8083;
+const port = 8083;
 const app = express();
 
+app.use([cors(), express.json()]);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/plan", planRouter);
 
-app.use([cors(),express.json()]);
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.listen(port, () => {console.log( `Server is Running on Port: http://localhost:${port}`); })
+app.listen(port, () => {
+  console.log(`Server is Running on Port: http://localhost:${port}`);
+});
